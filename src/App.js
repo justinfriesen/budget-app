a0063938a49e55b5975422596d110b126fe44634
Justin <justinfriesen30@gmail.com>
2022-06-16 11:40:40 -0400

    lots of updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Stack, Button } from "react-bootstrap"
+import BudgetCard  from "./components/BudgetCard"
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useState } from "react"
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  <>
+    <Container className="my-4"> {/* Gives spacing on top and bottom of page */}
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto"> Budgets</h1> {/* Margin on edge of budgets will eb auto */}
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}> Add Budget </Button>
+        <Button variant="outline-primary"> Add Expense </Button>
+      </Stack>
+      <div style={{
+        display:"grid", 
+        gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))", 
+        gap:"1rem", 
+        alignItems:"flex-start"}}> 
+        <BudgetCard name="Entertainment" gray amount={100} max={1000}>
+
+        </BudgetCard>
+        {/* Gap between items */}
+        {/* Margin on edge of budgets will eb auto */}
+        {/* Min size of all cards = 300px, takes up 1fr max space*/}
+        {/* Margin on edge of budgets will eb auto */}
+
+      </div>
+
+    </Container>
+    <AddBudgetModal show={showAddBudgetModal} handleClose={() =>
+    setShowAddBudgetModal(false)}/>
+  </>
+  )
+
 }
 
 export default App;
---
+++ src/contexts/BudgetsContext.js
@@ -0,0 +1,61 @@
+import React, {useContext, useState} from "react"
+import {v4 as uuidV4} from 'uuid'
+import useLocalStorage from "../hooks/useLocalStorage"
+//Contexts allow us to pass around information much easier.
+const BudgetContext = React.createContext()
+
+
+export function useBudgets(){
+    return useContext(BudgetContext)
+
+}
+
+export const BudgetsProvider = ({ children }) => {
+    const [budgets, setBudgets] = useLocalStorage("budgets", [])
+    const [expenses, setExpenses] = useLocalStorage("expenses", [])
+
+
+    function getBudgetExpenses(budgetId){
+        return expenses.filter(expense => expense.budgetId === budgetId)
+
+    }
+    function addExpense({description, amount, budgetId}){
+        setExpenses(prevExpenses =>{
+            return[...prevExpenses, { id: uuidV4(), description, amount, budgetId}]
+        })
+
+    }
+
+    function addBudget({name, max}){
+        setBudgets(prevBudgets =>{
+            if (prevBudgets.find(budget => budget.name === name)){
+                return prevBudgets
+            }
+            return[...prevBudgets, { id: uuidV4(), name, max}]
+        })
+    }
+    function deleteBudget({ id }){
+        //TODO deal with uncategorized expenses
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+
+    }
+    function deleteExpense({ id }){
+        setBudgets(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
+
+    }
+
+    return <BudgetContext.Provider value={{
+        budgets, 
+        expenses, 
+        getBudgetExpenses,
+        addBudget, 
+        addExpense,
+        deleteBudget,
+        deleteExpense
+    }}> {children} </BudgetContext.Provider>
+
+};
